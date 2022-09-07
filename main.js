53d7f771b717fc0f47dd9425acb9cb48722a7f51
mjlondono <mjllondono@gmail.com>
2022-09-07 14:59:16 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,84 @@
+
+function menuPrincipal(){
+
+    var menu;
+
+    menu = document.getElementById("div-login")
+    menu.style.display="none";
+
+    menu = document.getElementById("div-menuRegistrarUsuario")
+    menu.style.display="none";
+
+    menu = document.getElementById("div-menuIngresarElemento")
+    menu.style.display="none";
+
+    menu = document.getElementById("div-menuBuscarElemento")
+    menu.style.display="none";
+
+    menu = document.getElementById("div-registrarUsuario")
+    menu.style.display="none";
+    
+    menu = document.getElementById("div-ingresarElemento")
+    menu.style.display="none";    
+
+    menu = document.getElementById("div-buscarElemento")
+    menu.style.display="none";   
+
+
+    menu = document.getElementById("div-menuPrincipal");
+    menu.style.display="inline";  
+
+}
+
+function cerrarSesion(){
+
+    var menu;
+    menu = document.getElementById("div-menuPrincipal")
+    menu.style.display="none";
+
+    menu = document.getElementById("div-login")
+    menu.style.display="inline";
+
+}
+
+function registrarUsuario(){
+
+    var menu;
+    menu = document.getElementById("div-menuPrincipal")
+    menu.style.display="none";
+
+    menu = document.getElementById("div-menuRegistrarUsuario")
+    menu.style.display="inline";
+
+    menu = document.getElementById("div-registrarUsuario")
+    menu.style.display="inline";
+
+}
+
+function ingresarElemento(){
+
+    var menu;
+    menu = document.getElementById("div-menuPrincipal")
+    menu.style.display="none";
+
+    menu = document.getElementById("div-menuIngresarElemento")
+    menu.style.display="inline";
+
+    menu = document.getElementById("div-ingresarElemento")
+    menu.style.display="inline";
+
+}
+
+function buscarElemento(){
+
+    var menu;
+    menu = document.getElementById("div-menuPrincipal")
+    menu.style.display="none";
+
+    menu = document.getElementById("div-menuBuscarElemento")
+    menu.style.display="inline";
+
+    menu = document.getElementById("div-buscarElemento")
+    menu.style.display="inline";
+
+}

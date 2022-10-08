34fc8c3c458bfa3e8f51e19417196e73cf6eb5fc
mjlondono <111097515+mjlondono@users.noreply.github.com>
2022-10-08 14:32:46 -0500

    Delete src directory

--- src/main/java/com/siet/app/repository/EstadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.siet.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.siet.app.model.Estado;
-
-public interface EstadoRepository extends JpaRepository<Estado,Integer>{
-    
-}
